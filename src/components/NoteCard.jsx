a176742522ae210fb5dc6b2a899ce5748409f897
Nuha Fadh <fayn.deco@gmail.com>
2023-09-26 14:47:10 +0700

    update button done

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,9 +1,29 @@
 "use client"
 
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export const NoteCard = ({ id, content }) => {
     const router = useRouter();
+    const [newContent, setNewContent] = useState(content);
+    const [editMode, setEditMode] = useState(false);
+
+    async function handleUpdateNote() {
+        const res = await fetch(
+            `https://devscale-mockapi.fly.dev/api/collections/notes/records/${id}`,
+        {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ content: newContent })
+        }
+        );
+        const data = await res.json();
+        console.log(data);
+        setEditMode(false);
+        router.refresh();
+    }
 
     async function handleDeleteNote() {
         const res = await fetch(`https://devscale-mockapi.fly.dev/api/collections/notes/records/${id}`, {
@@ -14,10 +34,21 @@ export const NoteCard = ({ id, content }) => {
     
     return (
         <div className="border-2 p-4 rounded-lg shadow">
-            <div className=" min-h-[120px]">{content}</div>
+            {editMode ? (
+                <input
+                value={ newContent || content }
+                onChange={(e)=> setNewContent(e.target.value)}
+                />
+            ) : (
+                <div className=" min-h-[120px]">{content}</div>
+            )}
             <div className="flex gap-4">
-                <button>Edit</button>
-                <button onClick={handleDeleteNote}>Delete</button>
+                {editMode ? (
+                    <button onClick={handleUpdateNote}>Update</button>
+                ) : (
+                    <button onClick={()=> setEditMode(true)}>Edit</button>
+                )}
+             <button onClick={handleDeleteNote}>Delete</button>
             </div>
         </div>
     )
