7c5c54e04de95b3f4a03c530657b80c449ed2617
Nuha Fadh <fayn.deco@gmail.com>
2023-09-26 15:13:37 +0700

    header done

--- src/components/NoteEditor.jsx
+++ src/components/NoteEditor.jsx
@@ -2,18 +2,22 @@
 
 import { Input } from "./Input"
 import { NoteCard } from "./NoteCard"
+import { Header } from "./header"
 
 export const NoteEditor = ({ notedData }) => {
     console.log(notedData)
     return (
-        <div className=" space-y-8">
-            <Input />
-            <div className="space-y-4">
-                {notedData.map(({ id, content })=>{
-                    return <NoteCard key={id} id={id} content={content}/>;
-                })}
+        <div>
+            <div className=" space-y-8">
+                <Header />
+                <Input />
+                <div className="space-y-4">
+                    {notedData.map(({ id, content })=>{
+                        return <NoteCard key={id} id={id} content={content}/>;
+                    })}
+                </div>
+                <NoteCard />
             </div>
-            <NoteCard />
         </div>
     )
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div>
+            <h2 className=" font-semibold text-4xl text-gray-900 dark:text-white ">To-do Today</h2>
+        </div>
+    )
+}
